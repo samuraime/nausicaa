e367d435df36f0f2da886fd5d8b3539c887de8f7
Minhui Zhao <samurai7@foxmail.com>
2020-10-14 15:29:07 +0800

    add failed tests

--- nausicaaUITests/nausicaaUITests.swift
+++ nausicaaUITests/nausicaaUITests.swift
@@ -27,7 +27,7 @@ class nausicaaUITests: XCTestCase {
         // UI tests must launch the application that they test.
         let app = XCUIApplication()
         app.launch()
-
+        XCTAssertNil(1, "hah")
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
