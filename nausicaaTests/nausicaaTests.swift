231a923efe4d683337dedeac2cfc88e672ea0004
Minhui Zhao <samurai7@foxmail.com>
2020-10-15 15:31:05 +0800

    fix test

--- nausicaaTests/nausicaaTests.swift
+++ nausicaaTests/nausicaaTests.swift
@@ -22,7 +22,6 @@ class nausicaaTests: XCTestCase {
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-        XCTAssert(1 == 0)
     }
 
     func testPerformanceExample() throws {
